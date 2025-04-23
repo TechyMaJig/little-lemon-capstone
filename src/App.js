1af39451b69e38e2eaee1ec5eb7f35c778d3b8af
TechyMaJig <decker.kathryn@gmail.com>
2025-04-23 07:46:44 -0500

    Refined hero layout, improved image positioning and responsiveness.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer'
+import Hero from './Hero';
 
 function App() {
   return (
   <>
   <Header />
+  <Hero />
   <Main />
   <Footer />
   </>
